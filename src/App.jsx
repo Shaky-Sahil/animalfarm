31efb8230ed38c5e507607cbb0c7843707688bdb
Sahil Shaikh <sahilpk81@gmail.com>
2022-12-29 13:15:42 +0530

    added ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+function useAnimalSearch(){
+ const [ animals , setAnimals ] = useState([]);
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery);  
+  },[])
+
+  const search = async (q) => {
+    
+    const response = await fetch(
+      'http://localhost:8080/?' + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+    setAnimals(data);
+    localStorage.setItem('lastQuery',q);
+  };
+  return {search,animals}
+}
 
 function App() {
   
+  const { search , animals } = useAnimalSearch();
+
   return (
-    <div>
-      Hi
-    </div>
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input
+        type="text"
+        placeholder="search"
+        onChange = {(e)=> search(e.target.value)}
+        />
+
+      <ul>
+       {animals.map((animal)=> (
+        <Animal key = {animal.id} {...animal} />
+       ))}
+       {animals.length === 0 && 'No animals found'}
+      </ul>
+    </main>
+  )
+}
+
+function Animal({type,name,age}){
+  return (
+    
+      <li>
+        <strong>{type}</strong>{name} ({age} years old)
+      </li>
+     
   )
 }
 
